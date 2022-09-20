db2a59bf187a7fc07aeccd280810c47bb0d10ab8
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-09-20 18:32:18 -0400

    Added new context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import './App.css'
+import { DataContext } from './context/DataContext'
+
 
 // function App (){}
 const App = () => {
@@ -37,7 +39,9 @@ const App = () => {
       <div>
           <h1 className='App'>{message}</h1>
           <SearchBar handleSearch={handleSearch} />
-          <Gallery data={data} />
+          <DataContext.Provider value={data} >
+          <Gallery />
+          </DataContext.Provider>
       </div>
   )
 }
